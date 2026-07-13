e561f5a772dedb4053af055a2a10aad9bf9e3761
Benjamin Benedek <benjamin.benedek@computacenter.com>
2026-07-13 13:30:42 +0200

    add more Tyvar effects

--- forge-gui/src/main/java/forge/gamemodes/rogue/effect/NPCEffect.java
+++ forge-gui/src/main/java/forge/gamemodes/rogue/effect/NPCEffect.java
@@ -78,7 +78,7 @@ public enum NPCEffect implements RogueEffect {
             return run.canAddCardAsCarryCard(getEffectCard());
         }
     },
-    TYVAR_RESPITE("npc_tyvar_respite", "Tyvar's Respite",
+    TYVAR_CAMP("npc_tyvar_camp", "Tyvar's Camp",
         ITEM_GAIN_DESCRIPTION, NPC.TYVAR,
         EffectType.ONESHOT, "Campfire") {
         @Override
@@ -91,7 +91,7 @@ public enum NPCEffect implements RogueEffect {
             return run.canAddCardAsCarryCard(getEffectCard());
         }
     },
-    TYVAR_LEADERSHIP("npc_tyvar_leadership", "Tyvar's Leadership",
+    TYVAR_SPHERE("npc_tyvar_sphere", "Tyvar's Sphere",
         ITEM_GAIN_DESCRIPTION, NPC.TYVAR,
         EffectType.ONESHOT, "Commander's Sphere") {
         @Override
@@ -104,7 +104,7 @@ public enum NPCEffect implements RogueEffect {
             return run.canAddCardAsCarryCard(getEffectCard());
         }
     },
-    TYVAR_LEGACY("npc_tyvar_legacy", "Tyvar's Legacy",
+    TYVAR_TOME("npc_tyvar_tome", "Tyvar's Tome",
         ITEM_GAIN_DESCRIPTION, NPC.TYVAR,
         EffectType.ONESHOT, "Tome of Legends|MKC|1") {
         @Override
@@ -117,61 +117,142 @@ public enum NPCEffect implements RogueEffect {
             return run.canAddCardAsCarryCard(getEffectCard());
         }
     },
-    TYVAR_BASTION_COMMANDER("npc_tyvar_bastion_commander", "Bastion Commander",
+    TYVAR_AUGMENTED("npc_tyvar_augmented", "Augmented",
         TRAIT_GAIN_DESCRIPTION, NPC.TYVAR,
-        EffectType.PERMANENT, "Tyvar Trait - Bastion Commander") {
+        EffectType.PERMANENT, "Tyvar Trait - Augmented") {
         @Override
         public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
             addEffectCardToCommandZone(human);
         }
     },
-    TYVAR_BLOODSWORN_COMMANDER("npc_tyvar_bloodsworn_commander", "Bloodsworn Commander",
+    TYVAR_BASTION("npc_tyvar_bastion", "Bastion",
         TRAIT_GAIN_DESCRIPTION, NPC.TYVAR,
-        EffectType.PERMANENT, "Tyvar Trait - Bloodsworn Commander") {
+        EffectType.PERMANENT, "Tyvar Trait - Bastion") {
         @Override
         public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
             addEffectCardToCommandZone(human);
         }
     },
-    TYVAR_AUGMENTED_COMMANDER("npc_tyvar_augmented_commander", "Augmented Commander",
+    TYVAR_BLOODSWORN("npc_tyvar_bloodsworn", "Bloodsworn",
         TRAIT_GAIN_DESCRIPTION, NPC.TYVAR,
-        EffectType.PERMANENT, "Tyvar Trait - Augmented Commander") {
+        EffectType.PERMANENT, "Tyvar Trait - Bloodsworn") {
         @Override
         public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
             addEffectCardToCommandZone(human);
+        }
+    },
+    TYVAR_BRAWLER("npc_tyvar_brawler", "Brawler",
+        TRAIT_GAIN_DESCRIPTION, NPC.TYVAR,
+        EffectType.PERMANENT, "Tyvar Trait - Brawler") {
+        @Override
+        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
+            addEffectCardToCommandZone(human);
+        }
+    },
+    TYVAR_CHARLATAN("npc_tyvar_charlatan", "Charlatan",
+        TRAIT_GAIN_DESCRIPTION, NPC.TYVAR,
+        EffectType.PERMANENT, "Tyvar Trait - Charlatan") {
+        @Override
+        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
+            addEffectCardToCommandZone(human);
+        }
+    },
+    TYVAR_CHEF("npc_tyvar_chef", "Chef",
+        TRAIT_GAIN_DESCRIPTION, NPC.TYVAR,
+        EffectType.PERMANENT, "Tyvar Trait - Chef") {
+        @Override
+        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
+            addEffectCardToCommandZone(human);
+        }
+    },
+    TYVAR_CRIMINAL("npc_tyvar_criminal", "Criminal",
+        TRAIT_GAIN_DESCRIPTION, NPC.TYVAR,
+        EffectType.PERMANENT, "Tyvar Trait - Criminal") {
+        @Override
+        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
+            addEffectCardToCommandZone(human);
+        }
+    },
+    TYVAR_CULTIST("npc_tyvar_cultist", "Cultist",
+        TRAIT_GAIN_DESCRIPTION, NPC.TYVAR,
+        EffectType.PERMANENT, "Tyvar Trait - Cultist") {
+        @Override
+        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
+            addEffectCardToCommandZone(human);
+        }
+    },
+    TYVAR_FLAME("npc_tyvar_flame", "Flame",
+        TRAIT_GAIN_DESCRIPTION, NPC.TYVAR,
+        EffectType.PERMANENT, "Tyvar Trait - Flame") {
+        @Override
+        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
+            addEffectCardToCommandZone(human);
+        }
+    },
+    TYVAR_SAGE("npc_tyvar_sage", "Sage",
+        TRAIT_GAIN_DESCRIPTION, NPC.TYVAR,
+        EffectType.PERMANENT, "Tyvar Trait - Sage") {
+        @Override
+        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
+            addEffectCardToCommandZone(human);
+        }
+    },
+    TYVAR_BEACON("npc_tyvar_beacon", "Tyvar's Beacon",
+        TRAIT_GAIN_DESCRIPTION + " ![[Command Beacon]]", NPC.TYVAR,
+        EffectType.PERMANENT, "Tyvar Trait - Beacon") {
+        @Override
+        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
+            addEffectCardToCommandZone(human);
+            RogueEffect.addCardToBattlefield("Command Beacon", human);
+        }
+    },
+    TYVAR_WAR_ROOM("npc_tyvar_war_room", "Tyvar's War Room",
+        TRAIT_GAIN_DESCRIPTION + " ![[War Room]]", NPC.TYVAR,
+        EffectType.PERMANENT, "Tyvar Trait - War Room") {
+        @Override
+        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
+            addEffectCardToCommandZone(human);
+            RogueEffect.addCardToBattlefield("War Room", human);
+        }
+    },
+    TYVAR_PALACE("npc_tyvar_palace", "Tyvar's Palace",
+        TRAIT_GAIN_DESCRIPTION + " ![[Opal Palace]]", NPC.TYVAR,
+        EffectType.PERMANENT, "Tyvar Trait - Palace") {
+        @Override
+        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
+            addEffectCardToCommandZone(human);
+            RogueEffect.addCardToBattlefield("Opal Palace", human);
         }
     },
 
     // Narset traits
-    NARSET_TRAVELER("npc_narset_traveler", "Traveler",
-        TRAIT_GAIN_DESCRIPTION + " ![[Fractured Powerstone]]", NPC.NARSET,
-        EffectType.PERMANENT, "Narset Trait - Traveler") {
+    NARSET_POWERSTONE("npc_narset_powerstone", "Narset's Powerstone",
+        ITEM_GAIN_DESCRIPTION, NPC.NARSET,
+        EffectType.ONESHOT, "Fractured Powerstone") {
         @Override
-        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
-            addEffectCardToCommandZone(human);
-            RogueEffect.addCardToBattlefield("Fractured Powerstone", human);
+        public void applyEffect(RogueRun run, EffectResultContext ctx) {
+            addEffectCardAsCarryCard(run, ctx, CarryCardType.ITEM);
         }
     },
-    NARSET_ALCHEMIST("npc_narset_alchemist", "Alchemist",
-        "Gain the {{Item}} %s.", NPC.NARSET,
+    NARSET_ELIXIR("npc_narset_alchemist", "Narset's Elixir",
+        ITEM_GAIN_DESCRIPTION, NPC.NARSET,
         EffectType.ONESHOT, "Ichor Elixir") {
         @Override
         public void applyEffect(RogueRun run, EffectResultContext ctx) {
             addEffectCardAsCarryCard(run, ctx, CarryCardType.ITEM);
         }
     },
-    NARSET_CHAOSBOUND("npc_narset_chaosbound", "Chaosbound",
-        TRAIT_GAIN_DESCRIPTION + " ![[Narset - Chaos Capsule]]", NPC.NARSET,
-        EffectType.PERMANENT, "Narset Trait - Chaosbound") {
+    NARSET_CHAOS_CAPSULE("npc_narset_chaos_capsule", "Narset's Chaos Capsule",
+        ITEM_GAIN_DESCRIPTION, NPC.NARSET,
+        EffectType.ONESHOT, "Narset Item - Chaos Capsule") {
         @Override
-        public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
-            addEffectCardToCommandZone(human);
-            RogueEffect.addCardToBattlefield("Narset - Chaos Capsule", human);
+        public void applyEffect(RogueRun run, EffectResultContext ctx) {
+            addEffectCardAsCarryCard(run, ctx, CarryCardType.ITEM);
         }
     },
-    NARSET_GOD_OF_CHAOS("npc_narset_god_of_chaos", "God of Chaos",
+    NARSET_CHAOSWALKER("npc_chaoswalker", "Chaoswalker",
         TRAIT_GAIN_DESCRIPTION, NPC.NARSET,
-        EffectType.PERMANENT, "Narset Trait - God of Chaos") {
+        EffectType.PERMANENT, "Narset Trait - Chaoswalker") {
         @Override
         public void onMatchStart(RegisteredPlayer human, RegisteredPlayer opponent, RogueRun run) {
             addEffectCardToCommandZone(human);
